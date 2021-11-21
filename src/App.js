4d8e367cd4d53e7f717168a82c9cf006b41afd78
Lucas Goulart <lucasgoulart@MacBook-Pro-de-Lucas.local>
2021-11-21 13:24:07 -0300

    Removendo toastify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-// import { toast } from 'react-toastify';
 
 import './App.css';
 import { DashBoard } from './components/Dashboard';
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ToastContainer } from 'react-toastify';
 
 import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
 
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <ToastContainer autoClose={3000} />
   </React.StrictMode>,
   document.getElementById('root')
 );
